ae94e8de27d5af4a0eb7604d95fae82967434907
Michael Brown <gamer5108@hotmail.com>
2012-07-22 21:37:42 -0600

    Added a contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 
 		it "should have the conent 'Sample App'" do
@@ -12,13 +14,13 @@ describe "Static pages" do
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
-																:text => "Ruby on Rails Tutorial Sample App | Home")
+																:text => "#{base_title} | Home")
 		end
 	end
 
 	describe "Help page" do
 
-		it "should have the content 'Help'" do
+		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('h1', :text => 'Help')
 		end
@@ -26,13 +28,13 @@ describe "Static pages" do
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('title',
-																:text => "Ruby on Rails Tutorial Sample App | Help")
+																:text => "#{base_title} | Help")
 		end
 	end
 
 	describe "About page" do
 
-		it "should have the content 'About Us'" do
+		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => 'About Us')
 		end
@@ -40,7 +42,21 @@ describe "Static pages" do
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
-																:text => "Ruby on Rails Tutorial Sample App | About Us")
+																:text => "#{base_title} | About Us")
+		end
+	end
+
+	describe "Contact page" do
+
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+																:text => "#{base_title} | Contact")
 		end
 	end
 
