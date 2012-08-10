8c3b4c2492aaea877b73f282f191a7ded83159c2
Michael Brown <gamer5108@hotmail.com>
2012-08-09 18:07:01 -0600

    add a full_title helper

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'jquery-rails', '2.0.0'
 
 group :test do
 	gem 'capybara', '1.1.2'
+	gem 'guard-spork', '0.3.2'
+	gem 'spork', '0.9.0'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,10 +11,10 @@ describe "Static pages" do
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
-		it "should have the title 'Home'" do
+		it "should not have a custom page title" do
 			visit '/static_pages/home'
-			page.should have_selector('title',
-																:text => "#{base_title} | Home")
+			page.should_not have_selector('title',
+																:text => '| Home')
 		end
 	end
 
